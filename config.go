0dca7915404c7f6dfaa563de6c822e581c489e16
Michael Fuller <mfuller@lambdal.com>
2025-10-24 16:44:13 -0500

    config: begin adoption of modules
    Similar to the blackbox_exporter, we want to support a
    user-defined list of modules that the redfish_exporter may support.
    
    Begins along on that path, by:
    
    - Refactoring "reading a config" so it is less coupled to a
    file. Take instead an io.Reader and use that in YAML unmarshal, so we
    can write bits of config directly in tests. (or *shudders*
    config-via-remote-thing-that-can-be-read, some day)
    - Implement 'default' values, which we know we'll need soon, following
    how blackbox_exporter does it via UnmarshalYAML-ing. As a treat, begin
    with a default value around collection (context) deadline which is not
    wired down into anything yet, but will be in following commits
    - Writes some quick tests around assumptions on user-defined config

--- config.go
+++ config.go
@@ -2,17 +2,65 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"sync"
+	"time"
 
 	"os"
 
-	yaml "gopkg.in/yaml.v2"
+	yaml "gopkg.in/yaml.v3"
 )
+
+var (
+	DefaultGPUCollector = GPUCollectorConfig{
+		CollectionDeadlineDuration: 30 * time.Second,
+	}
+	DefaultModule = Module{
+		GPUCollector: DefaultGPUCollector,
+	}
+)
+
+type GPUCollectorConfig struct {
+	CollectionDeadlineDuration time.Duration `yaml:"collection_deadline"`
+}
+
+func (g *GPUCollectorConfig) UnmarshalYAML(unmarshal func(any) error) error {
+	*g = DefaultGPUCollector
+	type plain GPUCollectorConfig
+
+	if err := unmarshal((*plain)(g)); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type Module struct {
+	GPUCollector GPUCollectorConfig `yaml:"gpu_collector"`
+}
+
+func (m *Module) UnmarshalYAML(unmarshal func(any) error) error {
+	*m = DefaultModule
+	type plain Module
+	if err := unmarshal((*plain)(m)); err != nil {
+		return err
+	}
+	return nil
+}
 
 type Config struct {
 	Hosts    map[string]HostConfig `yaml:"hosts"`
 	Groups   map[string]HostConfig `yaml:"groups"`
 	Loglevel string                `yaml:"loglevel"`
+	Modules  map[string]Module     `yaml:"modules"`
+}
+
+func (c *Config) UnmarshalYAML(unmarshal func(any) error) error {
+	type plain Config
+	if err := unmarshal((*plain)(c)); err != nil {
+		return err
+	}
+	return nil
 }
 
 type SafeConfig struct {
@@ -26,15 +74,19 @@ type HostConfig struct {
 }
 
 // Read exporter config from file
-func NewConfigFromFile(configFile string) (*Config, error) {
-	var config = &Config{}
-	yamlFile, err := os.ReadFile(configFile) //nolint:gosec // Config path must be user-specifiable for deployment
+func NewConfigFromFile(configFilePath string) (*Config, error) {
+	file, err := os.Open(configFilePath)
+	defer file.Close()
 	if err != nil {
 		return nil, err
 	}
+	return readConfigFrom(file)
+}
 
-	if err := yaml.Unmarshal(yamlFile, config); err != nil {
-		return nil, err
+func readConfigFrom(r io.Reader) (*Config, error) {
+	config := &Config{}
+	if err := yaml.NewDecoder(r).Decode(config); err != nil {
+		return config, err
 	}
 
 	return config, nil
--- config_test.go
+++ config_test.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+	gta "gotest.tools/v3/assert"
+	"gotest.tools/v3/assert/cmp"
 )
 
 func TestConfigFromFile(t *testing.T) {
@@ -16,4 +20,90 @@ func TestConfigFromFile(t *testing.T) {
 	assert.Equal(t, "info", config.Loglevel)
 	assert.Equal(t, config.Hosts["default"], HostConfig{Username: "user", Password: "pass"})
 	assert.Equal(t, config.Groups["group1"], HostConfig{Username: "group1_user", Password: "group1_pass"})
+}
+
+func TestModulesConfig(t *testing.T) {
+	tT := map[string]struct {
+		inputYAML     string
+		wantErrString string
+		wantConfig    *Config
+	}{
+		"GPU Collector with a deadline": {
+			inputYAML: `
+modules:
+  foo:
+    gpu_collector:
+      collection_deadline: 60s
+`,
+			wantErrString: "",
+			wantConfig: &Config{
+				Modules: map[string]Module{
+					"foo": {
+						GPUCollector: GPUCollectorConfig{
+							CollectionDeadlineDuration: 60 * time.Second,
+						},
+					},
+				},
+			},
+		},
+		"GPU Collector without deadline gets a default": {
+			inputYAML: `
+modules:
+  foo:
+    gpu_collector:
+`,
+			wantErrString: "",
+			wantConfig: &Config{
+				Modules: map[string]Module{
+					"foo": {
+						GPUCollector: GPUCollectorConfig{
+							CollectionDeadlineDuration: 30 * time.Second,
+						},
+					},
+				},
+			},
+		},
+		"Unmarshal of multiple modules and with log level looks OK": {
+			inputYAML: `
+loglevel: info
+modules:
+  foo:
+    gpu_collector:
+  boo:
+    gpu_collector:
+      collection_deadline: 10s
+`,
+			wantErrString: "",
+			wantConfig: &Config{
+				Loglevel: "info",
+				Modules: map[string]Module{
+					"foo": {
+						GPUCollector: GPUCollectorConfig{
+							CollectionDeadlineDuration: 30 * time.Second,
+						},
+					},
+					"boo": {
+						GPUCollector: GPUCollectorConfig{
+							CollectionDeadlineDuration: 10 * time.Second,
+						},
+					},
+				},
+			},
+		},
+		"erroneous config returns error": {
+			inputYAML:     `foo:bar:baz`,
+			wantErrString: "unmarshal errors:\n  line 1: cannot unmarshal !!str",
+			wantConfig:    &Config{},
+		},
+	}
+	for tName, test := range tT {
+		t.Run(tName, func(t *testing.T) {
+			byteReader := bytes.NewReader([]byte(test.inputYAML))
+			gotConfig, err := readConfigFrom(byteReader)
+			gta.Assert(t, cmp.DeepEqual(test.wantConfig, gotConfig))
+			if test.wantErrString != "" {
+				gta.ErrorContains(t, err, test.wantErrString)
+			}
+		})
+	}
 }
