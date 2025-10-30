11412029323f25f22d3593609db5655c8602e890
Michael Fuller <mfuller@lambdal.com>
2025-10-30 17:23:41 -0500

    collector/json: surface errors
    In cases where module configuration is erroneous or the results are not
    in a form that the JSON Collector expects, gather those errors and
    surface them back to the user.

--- collector/json_collector.go
+++ collector/json_collector.go
@@ -2,6 +2,7 @@ package collector
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"log/slog"
@@ -113,9 +114,11 @@ func (j *JSONCollector) Collect(ch chan<- prometheus.Metric) {
 // It is expected that the result of JQ application yields data in a format which may further be
 // converted to a typed struct. A slice of JSONYieldedMetric is returned then for all data which meets
 // this expectation.
-// An error during JQ parsing results in returning a nil slice and the encountered error.
+// An error during JQ parsing results skips the item, and errors encountered in this way
+// are joined together and returned as a bundle.
 func metricsFromBody(query *gojq.Query, jsonBody []byte) ([]JSONYieldedMetric, error) {
 	var yielded []JSONYieldedMetric
+	var parseErrors []error
 	var intermediary map[string]any
 
 	if err := json.Unmarshal(jsonBody, &intermediary); err != nil {
@@ -132,42 +135,64 @@ func metricsFromBody(query *gojq.Query, jsonBody []byte) ([]JSONYieldedMetric, e
 			if err, ok := err.(*gojq.HaltError); ok && err.Value() == nil {
 				break
 			}
-			return nil, err
+			return []JSONYieldedMetric{}, err
 		}
 		if container, ok := v.([]any); ok {
 			for _, items := range container {
 				if item, ok := items.(map[string]any); ok {
-					yielded = append(yielded, convertToMetric(item))
+					yieldedMetric, err := convertToMetric(item)
+					if err != nil {
+						parseErrors = append(parseErrors, err)
+						continue
+					}
+					yielded = append(yielded, yieldedMetric)
 				}
 			}
 		}
 	}
-	return yielded, nil
+	return yielded, errors.Join(parseErrors...)
 }
 
 // converttoMetric yields a typed struct generated through type assertions
 // on an item, where the item was generated elsewhere by applying JQ expressions against Redfish
 // data. The exact desired input format is documented more completely
 // in the exporter's user documentation.
-func convertToMetric(item map[string]any) JSONYieldedMetric {
+func convertToMetric(item map[string]any) (JSONYieldedMetric, error) {
 	ret := JSONYieldedMetric{
 		Labels: map[string]string{},
 	}
+	var convertErrors []error
+	keys := slices.Sorted(maps.Keys(item))
+
 	if iName, ok := item["name"]; ok {
 		if strName, ok := iName.(string); ok {
 			ret.Name = strName
+		} else {
+			convertErrors = append(convertErrors, fmt.Errorf("item contained a non-string name"))
 		}
+	} else {
+		convertErrors = append(convertErrors, fmt.Errorf("item missing name, provided keys: %s", keys))
 	}
+
 	if iVal, ok := item["value"]; ok {
 		if floatVal, ok := iVal.(float64); ok {
 			ret.Value = floatVal
+		} else {
+			convertErrors = append(convertErrors, fmt.Errorf("item contained a non-float value"))
 		}
+	} else {
+		convertErrors = append(convertErrors, fmt.Errorf("item missing value, provided keys: %s", keys))
 	}
 	if iHelp, ok := item["help"]; ok {
 		if strHelp, ok := iHelp.(string); ok {
 			ret.Help = strHelp
+		} else {
+			convertErrors = append(convertErrors, fmt.Errorf("item contained a non-string help"))
 		}
+	} else {
+		convertErrors = append(convertErrors, fmt.Errorf("item missing help, provided keys: %s", keys))
 	}
+
 	if iLabels, ok := item["labels"]; ok {
 		if mapLabels, ok := iLabels.(map[string]any); ok {
 			for lName, lVal := range mapLabels {
@@ -177,5 +202,6 @@ func convertToMetric(item map[string]any) JSONYieldedMetric {
 			}
 		}
 	}
-	return ret
+
+	return ret, errors.Join(convertErrors...)
 }
--- collector/json_collector_test.go
+++ collector/json_collector_test.go
@@ -52,6 +52,36 @@ func Test_metricsFromBody(t *testing.T) {
 				},
 			},
 		},
+		"errors are bubbled up": {
+			rawBody: []byte(`{
+"Oem": {
+  "deltaenergysystems": {
+    "AllSensors": {
+      "Sensors": [{
+        "DataSourceUri": "/redfish/v1/Chassis/PowerShelf_0/Sensors/hotswap_temp",
+        "DeviceName": "hotswap_temp",
+        "Reading": 43.0,
+        "ReadingType": "Temperature",
+        "ReadingUnits": "Cel"
+      }]
+    }
+  }
+}}`),
+			jqFilter: `[.Oem.deltaenergysystems.AllSensors.Sensors[]] | map({
+        name1: (if .DeviceName | test("^ps[0-9]+_") then .DeviceName | sub("^ps[0-9]+_"; "") else .DeviceName end),
+        valuefoo: .Reading,
+        labels: (
+          if .DeviceName | test("^ps[0-9]+_") then {"power_supply_id": (.DeviceName | split("_")[0])}
+          else {}
+          end),
+          _raw: .
+      }) |
+      map(
+        .help = "Value yielded from the Redfish API endpoint: " + ._raw.DataSourceUri + ",type: " + ._raw.ReadingType + ",unit: " + ._raw.ReadingUnits) |
+        map(del(._raw)) | sort_by(.name)`,
+			wantErrString: "item missing name, provided keys: [help labels name1 valuefoo]",
+			wantMetrics:   nil,
+		},
 	}
 	for tName, test := range tT {
 		t.Run(tName, func(t *testing.T) {
@@ -74,18 +104,21 @@ func Test_convertToMetric(t *testing.T) {
 	tT := map[string]struct {
 		item       map[string]any
 		wantMetric JSONYieldedMetric
+		wantError  string
 	}{
 		"normal, no labels": {
 			item: map[string]any{
 				"name":  "foo",
 				"value": 1.0,
+				"help":  "bar",
 			},
 			wantMetric: JSONYieldedMetric{
 				Name:   "foo",
-				Help:   "",
+				Help:   "bar",
 				Value:  1.0,
 				Labels: map[string]string{},
 			},
+			wantError: "",
 		},
 		"normal, labels and help": {
 			item: map[string]any{
@@ -104,13 +137,38 @@ func Test_convertToMetric(t *testing.T) {
 					"tree": "house",
 				},
 			},
+			wantError: "",
+		},
+		"unexpected input leads to empty metric and error": {
+			item: map[string]any{
+				"name":  1.0,
+				"value": "foo",
+			},
+			wantMetric: JSONYieldedMetric{
+				Labels: map[string]string{},
+			},
+			wantError: "item contained a non-string name",
+		},
+		"missing input leads to empty metric and error": {
+			item: map[string]any{
+				"foo": "name",
+			},
+			wantMetric: JSONYieldedMetric{
+				Labels: map[string]string{},
+			},
+			wantError: "item missing name, provided keys: [foo]\nitem missing value, provided keys: [foo]\nitem missing help, provided keys: [foo]",
 		},
 	}
 
 	for tName, test := range tT {
 		t.Run(tName, func(t *testing.T) {
-			got := convertToMetric(test.item)
+			got, err := convertToMetric(test.item)
 			gta.Assert(t, cmp.DeepEqual(test.wantMetric, got))
+			if test.wantError != "" {
+				gta.Assert(t, cmp.ErrorContains(err, test.wantError))
+			} else {
+				gta.NilError(t, err)
+			}
 		})
 	}
 }
